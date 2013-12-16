d0bce49d78971b2292605533d2dc4d1f5bbd8d30
kislo_metal <kislo.metal@gmail.com>
2013-12-16 22:23:39 +0200

    adding spring support

--- src/com/diosoft/sample/calendar/Main.java
+++ src/com/diosoft/sample/calendar/Main.java
@@ -4,6 +4,8 @@ import com.diosoft.sample.calendar.datastore.CalendarDataStore;
 import com.diosoft.sample.calendar.datastore.CalendarDataStoreImpl;
 import com.diosoft.sample.calendar.service.CalendarService;
 import com.diosoft.sample.calendar.service.CalendarServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.logging.Logger;
 
@@ -12,9 +14,9 @@ public class Main {
     public static final Logger logger = Logger.getAnonymousLogger();
 
     public static void main(String[] args) {
-        CalendarDataStore dataStore = new CalendarDataStoreImpl();
 
-        CalendarService service = new CalendarServiceImpl(dataStore);
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CalendarService service = (CalendarService) context.getBean("calendarService");
 
         for (String name : args)
             service.addEvent(name, name, null, null, null);
